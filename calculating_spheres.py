b59b81d889f7d838d28b41a62992b1f5589bedb6
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-11-28 16:08:51 +0000

    Removed surface area function

--- calculating_spheres.py
+++ calculating_spheres.py
@@ -2,24 +2,21 @@
 
 # Created by Cristiano Sellitto
 # Created in November 2022
-# A program that calculates a sphere's area and volume
+# A program that calculates a sphere's volume
 
 import math
 
 
-def main():
-    # Calculates a sphere's area and volume
+def volume_of_a_sphere():
+    # Calculates a sphere's volume
 
-    print("\nThis program calculates a sphere's surface area and volume")
-    print("A = 4πr²")
+    print("\nThis program calculates a sphere's volume.")
     print("V = (4 ÷ 3) × πr³")
     try:
         radius_text = input("\nEnter the sphere's radius: ")
         radius_number = float(radius_text)
-        surface_area = 4 * math.pi * radius_number**2
         volume = (4 / 3) * math.pi * radius_number**3
-        print("\nThis sphere's surface area is {}cm²".format(surface_area))
-        print("This sphere's volume is {}cm³".format(volume))
+        print("\nThis sphere's volume is {}cm³".format(volume))
     except ValueError:
         print("\nYou did not input a number. (ValueError)")
     finally:
@@ -27,4 +24,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    volume_of_a_sphere()
