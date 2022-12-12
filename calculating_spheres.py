d0f0b854ecbdcb9dffae36c0b957b574652ca3b4
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-12-12 16:20:17 +0000

    Changed functions

--- calculating_spheres.py
+++ calculating_spheres.py
@@ -7,16 +7,24 @@
 import math
 
 
-def volume_of_a_sphere():
+def volume_of_a_sphere(radius):
     # Calculates a sphere's volume
+
+    volume = (4 / 3) * math.pi * radius**3
+
+    return volume
+
+
+def main():
+    # Gets input and calls the function above
 
     print("\nThis program calculates a sphere's volume.")
     print("V = (4 ÷ 3) × πr³")
     try:
         radius_text = input("\nEnter the sphere's radius: ")
         radius_number = float(radius_text)
-        volume = (4 / 3) * math.pi * radius_number**3
-        print("\nThis sphere's volume is {}cm³".format(volume))
+        volume_number = volume_of_a_sphere(radius_number)
+        print("\nThis sphere's volume is {}cm³".format(volume_number))
     except ValueError:
         print("\nYou did not input a number. (ValueError)")
     finally:
@@ -24,4 +32,4 @@ def volume_of_a_sphere():
 
 
 if __name__ == "__main__":
-    volume_of_a_sphere()
+    main()
